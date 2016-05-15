fffb57f6f7bd3687ffcfc85a316f770b7e1dd07b
4evterg <4evterg@gmail.com>
2016-05-15 23:20:39 +0300

    Create main layout + main activity

---
+++ DonGTU_mobile/app/src/main/java/com/chetverg/dongtu_mobile/MainActivity.java
@@ -0,0 +1,16 @@
+package com.chetverg.dongtu_mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chetverg on 15.05.16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
