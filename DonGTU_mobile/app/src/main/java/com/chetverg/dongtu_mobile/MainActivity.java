f3f012e6bda51b7aff288b0941199bf6695198ab
4evterg <4evterg@gmail.com>
2016-05-16 15:32:07 +0300

    Add menu navigation

--- DonGTU_mobile/app/src/main/java/com/chetverg/dongtu_mobile/MainActivity.java
+++ DonGTU_mobile/app/src/main/java/com/chetverg/dongtu_mobile/MainActivity.java
@@ -2,25 +2,24 @@ package com.chetverg.dongtu_mobile;
 
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-
-/**
- * Created by chetverg on 15.05.16.
- */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        InitNavigationView();
     }
 
     private void initToolbar(){
@@ -34,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void InitNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 
 }
